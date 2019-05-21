44543c17a44b32bf8d8f167fe2c573753477f508
marieauxille denis <marieauxille@gmail.com>
2019-05-21 16:16:13 +0200

    finished cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,5 @@
 
 p "destroying ingredients"
-
 Ingredient.destroy_all
 p "done"
 
@@ -11,6 +10,10 @@ Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
 Ingredient.create(name: "orange")
+# Ingredient.create(name: "rhum")
+# Ingredient.create(name: "vodka")
+# Ingredient.create(name: "lime juice")
+# Ingredient.create(name: "soda water")
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
